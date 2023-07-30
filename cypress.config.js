4ed1349516ec861997891b78a34f04821ef18211
Evgenii <evg.bazhanov@yahoo.com>
2023-07-30 08:30:50 +0200

    correction

--- cypress.config.js
+++ cypress.config.js
@@ -3,9 +3,11 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   viewportWidth: 1280,
   viewportHeight: 800,
-  defaultCommandTimeout: 29000,
   watchForFileChanges: false,
+  defaultCommandTimeout: 29000,
   e2e: {
+    baseUrl: "https://qa-assignment.stagingdesigner.com/",
+    testIsolation: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
